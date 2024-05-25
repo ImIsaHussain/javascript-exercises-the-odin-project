d4d267ea66ec26ec21cc3218c6ad6c0d67bad5d1
Isa Hussain <106785348+ImIsaHussain@users.noreply.github.com>
2024-05-25 13:45:57 -0400

    Complete exercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let arrayCopy = [...arguments[0]];
+    let returnArray = [];
 
+    for (let i = 0; i < arrayCopy.length; i++) {
+        let remove = false;
+        for (let j = 1; j < arguments.length; j++) {
+            let valueToRemove = arguments[j];
+            if (arrayCopy[i] === valueToRemove) {
+                remove = true;
+                break;
+            }
+        }
+        if (!remove) {
+            returnArray.push(arrayCopy[i]);
+        }
+    }
+
+    return returnArray;
 };
 
 // Do not edit below this line
