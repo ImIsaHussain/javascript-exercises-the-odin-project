8f2569fe1d198fda444edca31e4298f03219f5b9
Isa Hussain <106785348+ImIsaHussain@users.noreply.github.com>
2024-05-25 11:10:44 -0400

    Complete exercise three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function () {
+    word = arguments[0];
+    let result = '';
 
+    for (let i = word.length; i >= 0; i--) {
+        result += word.charAt(i);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
