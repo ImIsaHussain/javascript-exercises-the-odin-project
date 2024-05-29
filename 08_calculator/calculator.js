e8efc14c44eaab63796e9b658e3719f11342f19e
Isa Hussain <106785348+ImIsaHussain@users.noreply.github.com>
2024-05-28 21:45:08 -0400

    Complete exercise eight

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, value) => total + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce(function (total, current) {
+    return total * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  numFac = 1;
+
+  for (i = num; i > 0; i--) {
+    numFac *= i;
+  }
+
+  return numFac;
 };
 
 // Do not edit below this line
